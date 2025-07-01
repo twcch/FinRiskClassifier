2fce74adfafca697855940f79653cccc6f6cd1ae
Alan Hsieh <twcch1218@gmail.com>
2025-07-01 21:54:08 +0800

    feat: update data download and save function from API

--- scripts/fetch_raw_data.py
+++ scripts/fetch_raw_data.py
@@ -3,6 +3,8 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+import json
+import argparse
 from pathlib import Path
 from core.data import *
 
@@ -10,7 +12,10 @@ from core.data import *
 RAW_DATA_PATH = Path("data/raw/")
 
 
-def main():
+def main(config_path: str):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
     companies_df = fetch_us_companies()
     save_us_companies(RAW_DATA_PATH, companies_df)
 
@@ -25,5 +30,15 @@ def main():
 
     delete_temp_dir()
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/v1/config.json",
+        help="Path to the configuration file",
+    )
+
+    args = parser.parse_args()
+    main(args.config)
